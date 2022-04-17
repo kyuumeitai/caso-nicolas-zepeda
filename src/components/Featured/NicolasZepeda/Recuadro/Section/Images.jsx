bbb1039120a45cf20db78120c96407017722c91b
Álex Acuña Viera @ hinata-wsl2 <kyuumeitai@gmail.com>
2022-04-17 11:09:30 -0400

    images

---
+++ src/components/Featured/NicolasZepeda/Recuadro/Section/Images.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
+const Images = ({ which }) => {
+  const addZeroPrefix = number => {
+    if (number < 10) {
+      return `0${number}`
+    }
+    return number
+  }
+
+  const { allFile } = useStaticQuery(graphql`
+    query GatonIllustrations {
+      allFile(filter: { relativeDirectory: { eq: "imgs" } }) {
+        nodes {
+          name
+          childImageSharp {
+            gatsbyImageData(width: 400, formats: [WEBP, AVIF])
+          }
+        }
+      }
+    }
+  `)
+
+  const selectImage = which => {
+    const image = allFile.nodes.find(
+      node => node.name === `${addZeroPrefix(which)}`,
+    )
+    return image
+  }
+
+  return (
+    <div>
+      <GatsbyImage alt="" image={getImage(selectImage(which))} />
+    </div>
+  )
+}
+
+export default Images
--- src/components/Featured/NicolasZepeda/Recuadro/index.jsx
+++ src/components/Featured/NicolasZepeda/Recuadro/index.jsx
@@ -10,11 +10,6 @@ import {
   animate,
 } from 'framer-motion'
 
-import content from '@/content'
-
-import { useGetResizer } from '@/contexts/Resizer'
-
-import Marco from './Marco'
 import Section from './Section'
 
 const Container = styled.div`
@@ -26,17 +21,6 @@ const Container = styled.div`
   color: black;
 `
 
-const Screen = styled.div`
-  position: fixed;
-  height: 100vh;
-  top: 0;
-  width: 100vw;
-  left: 0;
-  text-align: center;
-  align-items: end;
-  justify-content: center;
-`
-
 const Content = styled(motion.div)`
   text-align: center;
   position: relative;
@@ -44,98 +28,22 @@ const Content = styled(motion.div)`
   padding: 2rem;
 `
 
-const MarcoWrap = styled.div`
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  overflow: hidden;
-  background-color: #ccc;
-`
-
-const MarcoBg = styled(motion.div)`
-  position: absolute;
-  height: 400vh;
-  width: 300vw;
-  will-change: transform;
-  svg {
-    width: 100%;
-  }
-`
-
-const SectionWrap = styled(motion.div)`
-  min-height: 200vh;
-`
+const SectionWrap = styled(motion.div)``
 
 const Recuadro = ({ chapters }) => {
-  const area = useGetResizer()
-  const containerRef = useRef()
-  const recuadroRef = useRef()
-  const [windowHeight, setWindowHeight] = useState(768)
-  const { scrollY } = useViewportScroll()
-  const recuadroXenY = useTransform(
-    scrollY,
-    [
-      0,
-      windowHeight,
-      windowHeight * 2,
-      windowHeight * 3,
-      windowHeight * 4,
-      windowHeight * 5,
-      windowHeight * 6,
-      windowHeight * 7,
-      windowHeight * 8,
-      windowHeight * 9,
-      windowHeight * 10,
-      windowHeight * 11,
-    ],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    { ease: easing.outQuad },
-  )
-
-  const recuadroYenY = useTransform(
-    scrollY,
-    [0, windowHeight * 10],
-    [0.01, 1],
-    {
-      ease: easing.outQuad,
-    },
-  )
-
-  const height = chapters.length // numero de secciones = numero de paños
-
   return (
-    <Container ref={containerRef} style={{ height: `${height * 100 * 2}vh` }}>
+    <Container>
       <Content className="container mx-auto ">
         {
           <AnimatePresence>
             {chapters.map((chapter, index) => (
               <SectionWrap key={index}>
-                <Section
-                  index={index}
-                  containerRef={containerRef}
-                  recuadroRef={recuadroRef}
-                  {...chapter}
-                />
+                <Section index={index} chapindex={index} {...chapter} />
               </SectionWrap>
             ))}
           </AnimatePresence>
         }
       </Content>
-      {/* 
-      <MarcoWrap>
-        <MarcoBg
-          key="marco"
-          ref={recuadroRef}
-          style={{
-            x: recuadroXenY,
-            y: recuadroYenY,
-            // scale: recuadroScaleEnY,
-          }}>
-          <Marco />
-        </MarcoBg>
-      </MarcoWrap> */}
     </Container>
   )
 }
